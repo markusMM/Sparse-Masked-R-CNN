2a4d0f2c4b7272a5b0f18f336028b75a640cdaf8
Nils-Markus Meister <meister.markus.MM@gmx.net>
2025-02-25 11:05:11 +0100

    nms

---
+++ nms/build.py
@@ -0,0 +1,35 @@
+import os
+import torch
+from torch.utils.cpp_extension import BuildExtension
+
+
+sources = ['src/nms.c']
+headers = ['src/nms.h']
+defines = []
+with_cuda = False
+
+if torch.cuda.is_available():
+    print('Including CUDA code.')
+    sources += ['src/nms_cuda.c']
+    headers += ['src/nms_cuda.h']
+    defines += [('WITH_CUDA', None)]
+    with_cuda = True
+
+this_file = os.path.dirname(os.path.realpath(__file__))
+print(this_file)
+extra_objects = ['src/cuda/nms_kernel.cu.o']
+extra_objects = [os.path.join(this_file, fname) for fname in extra_objects]
+
+ffi = BuildExtension(
+    headers=headers,
+    sources=sources,
+    define_macros=defines,
+    relative_to=__file__,
+    with_cuda=with_cuda,
+    extra_objects=extra_objects
+)
+
+if __name__ == '__main__':
+    ffi.build_extension(
+        '_ext.nms'
+    )
