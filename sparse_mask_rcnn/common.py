a9f42679b8a30b548a601469dd906e248269b93f
Nils-Markus Meister <meister.markus.MM@gmx.net>
2025-02-25 11:06:51 +0100

    config + common structure

---
+++ sparse_mask_rcnn/common.py
@@ -0,0 +1,18 @@
+import os
+from sparse_mask_rcnn.config import Config
+
+# Root directory of the project
+ROOT_DIR = os.getcwd()
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Path to trained weights file
+# Download this file and place in the root of your
+# project (See README file for details)
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.pth")
+
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+
+config = Config()
